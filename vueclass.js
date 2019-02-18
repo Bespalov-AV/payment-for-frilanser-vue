24825f65c8fd657a99363d676c8663166ee30748
Bespalov-AV <36464371+Bespalov-AV@users.noreply.github.com>
2019-02-18 21:09:28 +0700

    пример для работы с таблицей

--- vueclass.js
+++ vueclass.js
@@ -1,7 +1,7 @@
 const Payment = new PaymentClass();
 Payment.Init();
 
-const mainVue = new Vue({
+const clientVue = new Vue({
     el: "#aside-left",
     data: {
         Clients: Payment.Clients
@@ -15,3 +15,14 @@ const mainVue = new Vue({
     }
     
 });
+
+const tableVue = new Vue({
+    el: '#payment-table',
+    data: {
+      items: [
+        { xxx: 'aaa', yyy: '!!!', zzz: '111' },
+        { xxx: 'bbb', yyy: '???', zzz: '222' },
+        { xxx: 'ccc', yyy: ':::', zzz: '333' },
+      ],
+    },
+  });
